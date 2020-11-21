92c133524deefcd3c05fdcc091efc94352907b68
suyog <dsuyog2013@gmail.com>
2020-11-21 16:59:48 +0530

    Added first_name and last_name to form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  Stock.create(ticker: 'AAPL', name: 'Apple inc.')
+  Stock.create(ticker: 'AMZN', name: 'Amazon inc.')
+  Stock.create(ticker: 'GOOG', name: 'Google inc.')
